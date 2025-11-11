b4894a5af63dd903fc2a76144d4d09cae1bb033f
2shining <fetrafaneva@gmail.com>
2025-11-11 14:03:32 +0300

    ajout de la section hero

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,17 @@
 * Components
 */
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
-    return <Header />
+    return (
+    <> 
+        <Header />
+        <main>
+            <Hero />
+        </main>
+    </> 
+    )
 }
 
 export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,61 @@
+
+const Hero = () => {
+    return(
+        <section
+            id="" 
+            className="pt-28 lg:pt-36"
+        >
+            <div className="">
+
+                <div>
+
+                   <div className="">
+                        <figure className="">
+                            <img 
+                            src="/images/avatar-1.jpg" 
+                            width={40}
+                            height={40}
+                            alt="Fetra Faneva photo" 
+                            className="img-cover"
+                            />
+                        </figure>
+
+                        <div className="">
+                            <span className="">
+                                <span className=""></span>
+                            </span>
+
+                            Available for work
+                        </div>
+                    </div> 
+
+                    <h2 className="">
+                        Building Scalable Modern Websites for the Future
+                    </h2>
+
+                    <div className="">
+                        ButtonPrimary
+
+                        ButtonOutline
+                    </div>
+                </div>
+
+                <div className="">
+                    <figure className="">
+                        <img 
+                        src="/images/hero-banner.png" 
+                        width={656}
+                        height={800}
+                        alt="Fetra Faneva" 
+                        className="w-full"
+                        />
+                    </figure>
+                </div>
+
+            </div>
+
+        </section>
+    )
+}
+
+export default Hero
