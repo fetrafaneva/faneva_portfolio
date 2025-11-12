49382e585cdf6f909df806e11188402ee7d2758b
2shining <fetrafaneva@gmail.com>
2025-11-12 14:29:08 +0300

    modification de la section hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,15 +2,15 @@
 const Hero = () => {
     return(
         <section
-            id="" 
+            id="home" 
             className="pt-28 lg:pt-36"
         >
-            <div className="">
+            <div className="container items-center lg:grid lg:grid-cols-2 lg:gap-10">
 
                 <div>
 
-                   <div className="">
-                        <figure className="">
+                   <div className="flex items-center gap-3">
+                        <figure className="img-box w-9 h-9 rounded-lg">
                             <img 
                             src="/images/avatar-1.jpg" 
                             width={40}
@@ -20,16 +20,16 @@ const Hero = () => {
                             />
                         </figure>
 
-                        <div className="">
-                            <span className="">
-                                <span className=""></span>
+                        <div className="flex items-center gap-1.5 text-zinc-400 text-sm tracking-wide">
+                            <span className="relative w-2 h-2 rounded-full bg-emerald-400">
+                                <span className="absolute inset-0 rounded-full bg-emerald-400 animate-ping"></span>
                             </span>
 
                             Available for work
                         </div>
                     </div> 
 
-                    <h2 className="">
+                    <h2 className="headline-1">
                         Building Scalable Modern Websites for the Future
                     </h2>
 
