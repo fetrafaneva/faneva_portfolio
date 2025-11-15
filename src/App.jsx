ba73b6a1e9db0b60c2a33b4bb12c9ba8820ab9ac
2shining <fetrafaneva@gmail.com>
2025-11-15 08:44:57 +0300

    ajout de la section footer

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Skill from "./components/Skill";
 import Work from "./components/Work";
 import Review from "./components/Review";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
     return (
@@ -27,6 +28,7 @@ const App = () => {
             <Review />
             <Contact />
         </main>
+        <Footer />
     </> 
     )
 }
