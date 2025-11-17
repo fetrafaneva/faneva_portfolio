ce8143264ff75177ff7563755a5a300451f75add
2shining <fetrafaneva@gmail.com>
2025-11-17 11:29:24 +0300

    ajout du lien POST vers getform.io pour la recuperation d'email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -70,7 +70,7 @@ const Contact = () => {
                 </div>
 
                 <form 
-                action="" 
+                action="https://getform.io/f/bwnylnva" 
                 method="POST"
                 className=" xl:pl-10 2xl:pl-20"
                 >
