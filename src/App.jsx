07187439532babb9f7633f2fd1cfc1a5b35e9e58
2shining <fetrafaneva@gmail.com>
2025-12-20 09:03:02 +0300

    ajout du component design

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Work from "./components/Work";
 import Review from "./components/Review";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Design from "./components/Design";
 
 const App = () => {
   return (
@@ -24,7 +25,7 @@ const App = () => {
         <About />
         <Skill />
         <Work />
-        <Review />
+        <Design />
         <Contact />
       </main>
       <Footer />
